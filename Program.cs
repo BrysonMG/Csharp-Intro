df19b7cc61b02290bca39fb0119b3325b2bef225
Bryson Goins <brysonmg@gmail.com>
2021-05-21 12:12:04 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+Console.Write("First Name? ");
+string first = Console.ReadLine();
+
+Console.Write("Last Name? ");
+string last = Console.ReadLine();
+
+string name = first + " " + last;
+
+string reversed = string.Empty;
+
+for (int i = name.Length - 1; i >= 0; i--)
+{
+    reversed += name[i];
+}
+
+Console.WriteLine(reversed);
